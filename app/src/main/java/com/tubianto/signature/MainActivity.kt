b0cd98e65608cdfd0dd78f21b882e1a01cc19840
sadewawicak25 <>
2021-06-21 13:09:44 +0700

    Initial commit

---
+++ app/src/main/java/com/tubianto/signature/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.tubianto.signature
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.github.gcacace.signaturepad.views.SignaturePad
+
+class MainActivity : AppCompatActivity() {
+    private var WRITE_EXTERNAL_STORAGE_PERMISSION_CODE: Int = 1
+    private var READ_EXTERNAL_STORAGE_PERMISSION_CODE: Int = 2
+    private lateinit var mSignature: SignaturePad
+    private lateinit var mSignatureBitmap: Bitmap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkPermission()
+        init()
+    }
+
+    private fun checkPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when (PackageManager.PERMISSION_DENIED) {
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) -> {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        WRITE_EXTERNAL_STORAGE_PERMISSION_CODE
+                    )
+                }
+                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        READ_EXTERNAL_STORAGE_PERMISSION_CODE
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            WRITE_EXTERNAL_STORAGE_PERMISSION_CODE -> if (grantResults.isNotEmpty()) {
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "Anda perlu memberikan semua izin untuk menggunakan aplikasi ini.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+            READ_EXTERNAL_STORAGE_PERMISSION_CODE -> if (grantResults.isNotEmpty()) {
+                if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "Anda perlu memberikan semua izin untuk menggunakan aplikasi ini.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun init() {
+        mSignature = findViewById(R.id.signature_pad)
+    }
+
+    fun clickClear(view: View) {
+        mSignature.clear()
+    }
+
+    fun clickSave(view: View) {
+        mSignatureBitmap = mSignature.signatureBitmap
+        dialogSignature(mSignatureBitmap)
+    }
+
+    private fun dialogSignature(bitmap: Bitmap){
+        val builder = AlertDialog.Builder(this)
+        val factory = LayoutInflater.from(this)
+        val myView = factory.inflate(R.layout.dialog_signature, null)
+        val ivResult = myView.findViewById<ImageView>(R.id.iv_result)
+
+        builder.setView(myView)
+        builder.setTitle("Signature")
+        builder.setNegativeButton("Close"){ dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.setPositiveButton("Share"){ _, _ ->
+            share(bitmap, "Signature")
+        }
+        builder.show().withCenteredButtons()
+
+        bitmap.let {
+            Glide.with(this)
+                .load(it)
+                .into(ivResult)
+        }
+    }
+
+    private fun AlertDialog.withCenteredButtons() {
+        val positive = getButton(AlertDialog.BUTTON_POSITIVE)
+        val negative = getButton(AlertDialog.BUTTON_NEGATIVE)
+
+        //Disable the material spacer view in case there is one
+        val parent = positive.parent as? LinearLayout
+        parent?.gravity = Gravity.CENTER_HORIZONTAL
+        val leftSpacer = parent?.getChildAt(1)
+        leftSpacer?.visibility = View.GONE
+
+        //Force the default buttons to center
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        layoutParams.weight = 1f
+        layoutParams.gravity = Gravity.CENTER
+
+        positive.layoutParams = layoutParams
+        negative.layoutParams = layoutParams
+    }
+
+    private fun share(source: Bitmap, title: String){
+        val bitmapPath = MediaStore.Images.Media.insertImage(contentResolver, source, title, null)
+        Log.e("BITMAP PATH", bitmapPath)
+        val bitmapUri: Uri = Uri.parse(bitmapPath)
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "image/png"
+        intent.putExtra(Intent.EXTRA_STREAM, bitmapUri)
+        startActivity(Intent.createChooser(intent, "Bagikan Tanda Tangan melalui"))
+    }
+}
